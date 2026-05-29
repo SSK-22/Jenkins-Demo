ee7f5d3aaeb396eeb8d7fc2a07911ab3fa15bfe2
SSK-22 <satheeshtecht20@gmail.com>
2026-05-29 20:16:32 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo {
 public static void main(String[] args) {
-System.out.println("Hello Satheesh from Jenkins Java Pipeline");
+System.out.println("Hello Satheesh from Jenkins Java Pipeline2");
 }
 }
